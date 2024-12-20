3776c49f077553a548ee8c668d0dee0eec663b41
Crow281 <heretic24@proton.me>
2024-12-19 17:51:53 -0800

    Updated doc and demo for version 1.1.0
    Updated doc and demo for version 1.1.0. The biggest changes are:
    - A couple utility functions for if you need to hide other DOM elements
    while the LoadingScreen is running.
    - Replaced the unicode symbols for the cancel and try again buttons with
    SVG images.

--- sample/load-operations/assets/AwaitScriptExample-FsaVRjTe.js
+++
@@ -1,1 +0,0 @@
-import{L as t,b as n}from"./index-BjkeFVs2.js";const c=new t,e=document.createElement("script");e.async=!0;e.src="./Example/AwaitScript/App.js";n(c,e);setTimeout(function(){document.body.appendChild(e)},3e3);

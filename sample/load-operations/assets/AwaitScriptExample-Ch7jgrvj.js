158163da19e76f9dad1c28f9cf47a2f3766b711a
Crow281 <heretic24@proton.me>
2024-12-27 01:18:49 -0800

    Updated to 1.1.1 with firefox bug fix.

---
+++ sample/load-operations/assets/AwaitScriptExample-Ch7jgrvj.js
@@ -0,0 +1,1 @@
+import{L as t,b as n}from"./index-Be7_cM6z.js";const c=new t,e=document.createElement("script");e.async=!0;e.src="./Example/AwaitScript/App.js";n(c,e);setTimeout(function(){document.body.appendChild(e)},3e3);
